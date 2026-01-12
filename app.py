525a6319951cb61e4a26b38cf36b4d48e04a1342
Abdul <Abdulwrite77@gmail.com>
2026-01-12 22:36:13 +0100

    code correction

--- app.py
+++ app.py
@@ -112,21 +112,21 @@ with st.sidebar:
     st.caption("• Output: Fraud Probability")
     st.caption("• Risk Levels: Low / Medium / High")
     
-    # ---------------- Dummy Data ----------------
-    st.markdown("---")
-    st.markdown("### ℹ️ Get Dummy Data Here")
-    st.markdown(
-        """
-        <div style="text-align: center; font-size: 0.85em; color: #8a8a8a; line-height: 1.7em;">
-            🔬 Get Data Here:
-            <a href="https://app.box.com/s/yih10tldxgnagreecb0hlicnph8d2h6l/" target="_blank">
-            Data
-         
-            </a>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+   # ---------------- Dummy Data Link ----------------
+st.markdown("---")
+st.markdown("### ℹ️ Get Dummy Data Here")
+st.markdown(
+    """
+    <div style="text-align: center; font-size: 0.85em; color: #8a8a8a; line-height: 1.7em;">
+        🔬 Get Dummy Data: 
+        <a href="https://app.box.com/s/yih10tldxgnagreecb0hlicnph8d2h6l" target="_blank">
+            Download
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
     
 
 
